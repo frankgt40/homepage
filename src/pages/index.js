2d8c985148323d1b4319625274ccbcea2e7d9aa7
Cheng Cai <frankgt40@gmail.com>
2018-06-12 23:23:57 -0700

    m

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Welcome</h1>
-    <p>I am Ph.D. student at CS@UCLA.</p>
+    <p>I am Ph.D. student at CS@UCLA. This is my new personal website. Not long ago I decided to build my new website with <a href="">Gatsby.js</a>, my old website was deleted. This site is still under construction.</p>
   </div>
 )
 
