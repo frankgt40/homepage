868fa6c639cf2add7d2ce8630b49b2f3cfe83694
Cheng Cai <frankgt40@gmail.com>
2018-07-13 17:26:20 -0700

    Changed the menu title

--- src/components/menu.js
+++ src/components/menu.js
@@ -15,7 +15,7 @@ const Menu = () => (
 	<li><Link to="/">Home</Link></li>
 	<li><Link to="/about">About</Link></li>
 	<li><Link to="/publications">Publications</Link></li>
-	<li><Link to="/leetCode">刷题日记</Link></li>
+	<li><Link to="/leetCode">Blogs</Link></li>
 	</ul>
 	</div>
 )
